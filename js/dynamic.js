296a200760e0ba7a26eccdd2eeb032923cc383ff
Alexandr <justdealman@gmail.com>
2015-07-14 08:53:03 +0700

    update

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,5 +1,4 @@
-﻿$(document).ready(function() {
-if ( $('.filter').length > 0 ) {
+﻿if ( $('.filter').length > 0 ) {
 	var filterNum = $('section.list .filter .number > ul > li');
 	filterNum.children('span').bind('click', function(event) {
 		if ( $(this).parent().hasClass('active') ) {
@@ -22,9 +21,20 @@ if ( $('.filter').length > 0 ) {
 	filterNum.click(function(event){
 		event.stopPropagation();
 	});
+	if ( $('html').attr('lang') === 'ru' ) {
+		var months = [ 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь' ];
+		var days = [ 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс' ];
+		var start = 7;
+	}
+	if ( $('html').attr('lang') === 'en' ) {
+		var months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+		var days = [ 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su' ];
+		var start = 6;
+	}
 	$('.filter input.from, .filter input.to').datepicker({
-		monthNames: [ 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь' ],
-		dayNamesShort: [ 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс' ],
+		monthNames: months,
+		dayNamesMin: days,
+		firstDay: start,
 		prevText: '',
 		nextText: '',
 		dateFormat: 'dd.mm.yy',
@@ -34,10 +44,10 @@ if ( $('.filter').length > 0 ) {
 if ( $('section.list .core').length > 0 ) {
 	$('section.list .core ul li p.place[data-address]').hover(
 		function() {
-			$('body').append('<p class="bubble-address">'+$(this).attr('data-address')+'</p>');
+			$(this).parents('.core').append('<p class="bubble-address">'+$(this).attr('data-address')+'</p>');
 			$('.bubble-address').css({
-				'left': $(this).offset().left+'px',
-				'top': $(this).offset().top+'px'
+				'left': $(this).position().left+'px',
+				'top': $(this).position().top+'px'
 			});
 		},
 		function() {
@@ -48,7 +58,70 @@ if ( $('section.list .core').length > 0 ) {
 if ( $('input[type="checkbox"]').length > 0 ) {
 	$('input[type="checkbox"]').uniform();
 }
-$('input.phone').mask('+7 000 000 00 00', {placeholder: '+7'});
+function completeCheck() {
+	if ( $('input.phone').parent('.success').length > 0 && $('input.password').parent('.success').length > 0 && $('input.confirm').parent('.success').length > 0 ) {
+		$('section.form .done button.next').addClass('active');
+		$('section.form .done button.next.active').on('click', function() {
+			console.log('Активная кнопка');
+		});
+	}
+	else {
+		$('section.form .done button.next').removeClass('active');
+	}
+}
+$('input.phone').inputmask('+7 999 999 99 99', {
+	'oncomplete': function(){
+		$(this).parent().addClass('success');
+		completeCheck();
+	},
+	'onincomplete': function(){
+		$(this).parent().removeClass('success');
+	}
+});
+$('input.password').keyup(function() {
+	if ( $(this).val().length > 0 ) {
+		if ( $(this).val().length < 8 ) {
+			$(this).parent().removeClass('success').addClass('error');
+			$(this).parent().find('h6').remove();
+			$(this).parent().append('<h6 class="message">Пароль содержит менее 8 знаков</h6>');
+			$(this).parent().find('h6').css({
+				'margin-top': -$(this).parent().find('h6').outerHeight()/2+14+'px'
+			});
+		}
+		else {
+			if ( $(this).val().match(/((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]))/gm) ) {
+				$(this).parent().removeClass('error').addClass('success');
+				$(this).parent().find('h6').remove();
+			}
+			else {
+				$(this).parent().removeClass('success').addClass('error');
+				$(this).parent().find('h6').remove();
+				$(this).parent().append('<h6 class="message">Пароль должен содержать хотя бы одну заглавную и строчную букву, а также цифру</h6>');
+				$(this).parent().find('h6').css({
+					'margin-top': -$(this).parent().find('h6').outerHeight()/2+14+'px'
+				});
+			}
+		}
+	}
+	completeCheck();
+}).keyup();
+$('input.confirm').keyup(function() {
+	if ( $(this).val().length > 0 ) {
+		if ( $(this).val() == $('input.password').val() && $(this).val().length > 0 ) {
+			$(this).parent().removeClass('error').addClass('success');
+			$(this).parent().find('h6').remove();
+		}
+		else {
+			$(this).parent().removeClass('success').addClass('error');
+			$(this).parent().find('h6').remove();
+			$(this).parent().append('<h6 class="message">Пароли не совпадают</h6>');
+			$(this).parent().find('h6').css({
+				'margin-top': -$(this).parent().find('h6').outerHeight()/2+14+'px'
+			});
+		}
+	}
+	completeCheck();
+}).keyup();
 if ( $('.modal').length > 0 ) {
 	$('.modal').append('<span class="close"></span>');
 }
@@ -112,52 +185,41 @@ $('.modal .close, .fade').bind('click', function(event) {
 	$('.modal, .fade').stop(true,true).fadeOut(200);
 	event.preventDefault();
 });
-if ( $('.restrictions-edit').length > 0 ) {
+function editSelect(e) {
+	e.on('click', function(event) {
+		var t = $(this).parent();
+		if ( t.hasClass('active') ) {
+			t.removeClass('active');
+			t.parent('.select').css({
+				'z-index': '10'
+			});
+		}
+		else {
+			t.addClass('active');
+			t.parent('.select').css({
+				'z-index': '50'
+			});
+		}
+	});
+}
+function editSelectDefault() {
 	$('.restrictions-edit .select').each(function() {
 		var selected = $(this).find('span').attr('data-selected');
 		$(this).find('span').text($(this).find('li[data-option="'+selected+'"]').text());
 		$(this).find('li[data-option="'+selected+'"]').hide();
 	});
-	$('.restrictions-edit li .more').bind('click', function(event) {
-		$(this).parents('li').append('<div><ul class="select type1"><li><span data-selected="1"></span><ul><li data-option="1">Сезонное</li><li data-option="2">Евро</li><li data-option="3">Что-то еще</li></ul></li></ul><span class="hide"></span></div>');
+}
+function editSelectRemove() {
+	$('.restrictions-edit li .remove').on('click', function(event) {
+		$(this).parents('li').remove();
 		event.preventDefault();
 	});
-	$('.restrictions .add span').bind('click', function(event) {
-		$(this).parents('.restrictions').find('.empty').hide();
-		$(this).parent().siblings('.restrictions-edit').show();
-		$(this).parents('.restrictions').find('.restrictions-edit').append('<li><div><ul class="select type1"><li><span data-selected="3"></span><ul><li data-option="1">Дизельное топливо Арктическое</li><li data-option="2">Бензин Аи-92</li><li data-option="3">Бензин Аи-95</li></ul></li></ul><span class="more"></span><span class="input"><input type="text" value="1"><em>л</em></span><ul class="select type2"><li><span data-selected="1"></span><ul><li data-option="1">в день</li><li data-option="2">в неделю</li><li data-option="3">в месяц</li><li data-option="4">в год</li></ul></li></ul><span class="remove"></span></div></li>')
-		event.preventDefault();
-	});
-	$('.restrictions-edit .select span').bind('click', function(event) {
-		if ( $(this).parent().hasClass('active') ) {
-			$(this).parent().removeClass('active');
-			$(this).parents('.select').css({
-				'z-index': '10'
-			});
-		}
-		else {
-			$(this).parent().addClass('active');
-			$(this).parents('.select').css({
-				'z-index': '50'
-			});
-		}
-		event.preventDefault();
-	});
-	$('.restrictions-edit .select ul li').bind('click', function(event) {
-		var parent = $(this).parents('.select');
-		$(this).hide().siblings().show();
-		parent.find('span').text($(this).text());
-		parent.find('span').attr('data-selected', $(this).attr('data-option'));
-		parent.children('li').removeClass('active');
-		parent.css({
-			'z-index': '10'
-		});
-		event.preventDefault();
-	});
+}
+function editSelectDropHide() {
 	$('html').click(function() {
 		$('.restrictions-edit .select > li').removeClass('active');
 	});
-	$('.restrictions-edit .select').click(function(event){
+	$('.restrictions-edit .select').on('click', function(event){
 		if ( $('.restrictions-edit .select li.active').length > 1 ) {
 			$('.restrictions-edit .select').css({
 				'z-index': '10'
@@ -168,13 +230,58 @@ if ( $('.restrictions-edit').length > 0 ) {
 		}
 		event.stopPropagation();
 	});
-	$('.restrictions-edit li .remove').bind('click', function(event) {
-		$(this).parents('li').remove();
+}
+function editSelectChoose() {
+	$('.restrictions-edit .select ul li').on('click', function(event) {
+		var parent = $(this).parents('.select');
+		$(this).hide().siblings().show();
+		parent.find('span').text($(this).text());
+		parent.find('span').attr('data-selected', $(this).attr('data-option'));
+		parent.children('li').removeClass('active');
+		parent.css({
+			'z-index': '10'
+		});
 		event.preventDefault();
 	});
-	$('.restrictions-edit li .hide').bind('click', function(event) {
+}
+function editSelectHide()  {
+	$('.restrictions-edit li .hide').on('click', function(event) {
 		$(this).parent().remove();
 		event.preventDefault();
 	});
 }
-});
+function editSelectMore(e)  {
+	e.on('click', function(event) {
+		$(this).parents('li').find('div.new').removeClass('new');
+		$(this).parents('li').append('<div class="new"><ul class="select type1"><li><span data-selected="1"></span><ul><li data-option="1">Сезонное</li><li data-option="2">Евро</li><li data-option="3">Что-то еще</li></ul></li></ul><span class="hide"></span></div>');
+		editSelect($(this).parents('li').find('div.new .select span'));
+		editSelectDefault();
+		editSelectDropHide();
+		editSelectChoose();
+		editSelectHide();
+		event.preventDefault();
+	});
+}
+if ( $('.restrictions-edit').length > 0 ) {
+	editSelect($('.restrictions-edit .select span'));
+	editSelectDefault();
+	editSelectRemove();
+	editSelectChoose();
+	editSelectHide();
+	editSelectDropHide();
+	editSelectMore($('.restrictions li .more'));
+	$('.restrictions .add span').on('click', function(event) {
+		$(this).parents('.restrictions').find('.empty').hide();
+		$(this).parent().siblings('.restrictions-edit').show();
+		$(this).parents('.restrictions').find('.restrictions-edit').find('li').removeClass('new');
+		$(this).parents('.restrictions').find('.restrictions-edit').append('<li class="new"><div><ul class="select type1"><li><span data-selected="3"></span><ul><li data-option="1">Дизельное топливо Арктическое</li><li data-option="2">Бензин Аи-92</li><li data-option="3">Бензин Аи-95</li></ul></li></ul><span class="more"></span><span class="input"><input type="text" value="1"><em>л</em></span><ul class="select type2"><li><span data-selected="1"></span><ul><li data-option="1">в день</li><li data-option="2">в неделю</li><li data-option="3">в месяц</li><li data-option="4">в год</li></ul></li></ul><span class="remove"></span></div></li>');
+		editSelect($(this).parents('.restrictions').find('.restrictions-edit').find('li.new .select span'));
+		editSelectDropHide();
+		editSelectDefault();
+		editSelectRemove();
+		editSelectChoose();
+		editSelectHide();
+		editSelectMore($(this).parents('.restrictions').find('.restrictions-edit').find('li.new .more'));
+		event.preventDefault();
+	});
+}
